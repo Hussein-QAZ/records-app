9c46bebc6c723a7620f117446a049cbc78d2d97f
Hussein-QAZ <husein.qasim@dci-student.org>
2024-10-09 15:49:05 +0200

    new fix

--- client/src/contexts/Context.jsx
+++ client/src/contexts/Context.jsx
@@ -5,6 +5,7 @@ import { usersInitialState, usersReducer } from "../reducers/userReducer";
 import { getMyData } from "../api/usersApi";
 import { getCartData } from "../api/cartsApi";
 import { setAxiosDefaults } from "../utils/axiosConfig";
+import {getAllRecords} from "../api/recordsApi.js"
 
 export const DataContext = createContext();
 
@@ -26,6 +27,7 @@ export const DataProvider = ({ children }) => {
   useEffect(() => {
     setAxiosDefaults();
     getMyData(usersDispatch);
+    getAllRecords(recordsDispatch);
   }, []);
 
   useEffect(() => {
